88c77bfbe4ac278adadc690867895ee50a68c01b
Matheus Sartoretto <matheus.sartoretto@geopal-solutions.com>
2021-04-02 14:54:56 +0100

    fix linting clashes with prettier

--- src/App.vue
+++ src/App.vue
@@ -4,23 +4,23 @@
 </template>
 
 <script lang="ts">
-import { defineComponent } from "vue";
-import HeaderMenu from "@/components/HeaderMenu.vue";
+  import { defineComponent } from 'vue';
+  import HeaderMenu from '@/components/HeaderMenu.vue';
 
-export default defineComponent({
-  components: {
-    HeaderMenu,
-  },
-});
+  export default defineComponent({
+    components: {
+      HeaderMenu,
+    },
+  });
 </script>
 
 <style lang="scss" scoped>
-#nav > a {
-  text-decoration: underline transparent;
-  transition: text-decoration-color 300ms;
+  #nav > a {
+    text-decoration: underline transparent;
+    transition: text-decoration-color 300ms;
 
-  &:hover {
-    text-decoration-color: black;
+    &:hover {
+      text-decoration-color: black;
+    }
   }
-}
 </style>
--- src/components/HeaderMenu.vue
+++ src/components/HeaderMenu.vue
@@ -4,7 +4,7 @@
       v-for="item in menuItems"
       :key="item.title"
       :to="item.linkTo"
-      class="p-4"
+      class="p-3"
     >
       {{ item.title }}
     </router-link>
@@ -12,39 +12,39 @@
 </template>
 
 <script lang="ts">
-import { defineComponent, reactive } from "vue";
-import router from "@/router/index";
-import { IHeaderMenuItem } from "@/types";
+  import { defineComponent, reactive } from 'vue';
+  import router from '@/router/index';
+  import { IHeaderMenuItem } from '@/types';
 
-export default defineComponent({
-  setup() {
-    let menuItems: IHeaderMenuItem[] = reactive([]);
+  export default defineComponent({
+    setup() {
+      let menuItems: IHeaderMenuItem[] = reactive([]);
 
-    const buildMenuItems = (): void => {
-      menuItems = router.getRoutes().map(
-        (route): IHeaderMenuItem => {
-          const title = route.name ? route.name.toString() : "";
-          const linkTo = route.path ? route.path.toString() : "";
+      const buildMenuItems = (): void => {
+        menuItems = router.getRoutes().map(
+          (route): IHeaderMenuItem => {
+            const title = route.name ? route.name.toString() : '';
+            const linkTo = route.path ? route.path.toString() : '';
 
-          return { title, linkTo };
-        }
-      );
-    };
+            return { title, linkTo };
+          },
+        );
+      };
 
-    buildMenuItems();
+      buildMenuItems();
 
-    return { menuItems };
-  },
-});
+      return { menuItems };
+    },
+  });
 </script>
 
 <style lang="scss" scoped>
-#nav > a {
-  text-decoration: underline transparent;
-  transition: text-decoration-color 300ms;
+  #nav > a {
+    text-decoration: underline transparent;
+    transition: text-decoration-color 300ms;
 
-  &:hover {
-    text-decoration-color: black;
+    &:hover {
+      text-decoration-color: black;
+    }
   }
-}
 </style>
