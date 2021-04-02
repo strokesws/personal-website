6015ef85d56f5994c6a315f3a8f9bdd67e744722
Matheus Sartoretto <matheus.sartoretto@geopal-solutions.com>
2021-04-02 14:46:38 +0100

    convert header menu into a component

---
+++ src/components/HeaderMenu.vue
@@ -0,0 +1,50 @@
+<template>
+  <nav id="nav" class="p-4 flex justify-end">
+    <router-link
+      v-for="item in menuItems"
+      :key="item.title"
+      :to="item.linkTo"
+      class="p-4"
+    >
+      {{ item.title }}
+    </router-link>
+  </nav>
+</template>
+
+<script lang="ts">
+import { defineComponent, reactive } from "vue";
+import router from "@/router/index";
+import { IHeaderMenuItem } from "@/types";
+
+export default defineComponent({
+  setup() {
+    let menuItems: IHeaderMenuItem[] = reactive([]);
+
+    const buildMenuItems = (): void => {
+      menuItems = router.getRoutes().map(
+        (route): IHeaderMenuItem => {
+          const title = route.name ? route.name.toString() : "";
+          const linkTo = route.path ? route.path.toString() : "";
+
+          return { title, linkTo };
+        }
+      );
+    };
+
+    buildMenuItems();
+
+    return { menuItems };
+  },
+});
+</script>
+
+<style lang="scss" scoped>
+#nav > a {
+  text-decoration: underline transparent;
+  transition: text-decoration-color 300ms;
+
+  &:hover {
+    text-decoration-color: black;
+  }
+}
+</style>
