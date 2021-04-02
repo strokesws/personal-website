09d091546573483cd3edbf2ac885af73d1505cf8
Matheus Sartoretto <matheus.sartoretto@geopal-solutions.com>
2021-04-02 14:47:06 +0100

    replace current header with newly created component

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,18 @@
 <template>
-  <nav id="nav" class="p-4 flex justify-end">
-    <router-link to="/" class="p-4">Home</router-link>
-    <router-link to="/about" class="p-4">About Me</router-link>
-    <router-link to="/" class="p-4">My Timeline</router-link>
-  </nav>
+  <HeaderMenu />
   <router-view />
 </template>
+
+<script lang="ts">
+import { defineComponent } from "vue";
+import HeaderMenu from "@/components/HeaderMenu.vue";
+
+export default defineComponent({
+  components: {
+    HeaderMenu,
+  },
+});
+</script>
 
 <style lang="scss" scoped>
 #nav > a {
