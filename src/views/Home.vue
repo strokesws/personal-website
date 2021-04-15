0755186bdf02e81a14aaf2fc8a147ec5b429e49f
Matheus Sartoretto <matheus.sartoretto@geopal-solutions.com>
2021-04-15 23:29:54 +0100

    add dummy home data

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,28 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png" />
-    <HelloWorld msg="Welcome to Your Vue.js + TypeScript App" />
+  <div class="grid grid-cols-12">
+    <div class="col-span-7 p-4">
+      <h1 class="text-6xl">Matheus Sartoretto</h1>
+      <h2 class="text-4xl">Front-end Developer</h2>
+      <div class="mt-8">
+        <!-- TODO: transform links into icons with fontawesome -->
+        <a href="https://github.com/strokesws" target="_blank">GitHub</a>
+        <a href="https://linkedin.com/in/matheussartoretto/" target="_blank">
+          LinkedIn
+        </a>
+      </div>
+    </div>
+    <div class="col-span-2 p-4">
+      <PolaroidPicture></PolaroidPicture>
+    </div>
   </div>
 </template>
 
 <script lang="ts">
   import { defineComponent } from 'vue';
-  import HelloWorld from '@/components/HelloWorld.vue'; // @ is an alias to /src
+  import PolaroidPicture from '@/components/PolaroidPicture.vue';
 
   export default defineComponent({
     name: 'Home',
-    components: {
-      HelloWorld,
-    },
+    components: { PolaroidPicture },
   });
 </script>
