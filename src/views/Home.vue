3197f1cd353013d7f70060ea5719140b0c1187fb
Matheus Sartoretto <matheus.sartoretto@geopal-solutions.com>
2021-04-16 21:08:05 +0100

    make home style improvements

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,17 +1,25 @@
 <template>
-  <div class="grid grid-cols-12">
-    <div class="col-span-7 p-4">
-      <h1 class="text-6xl">Matheus Sartoretto</h1>
-      <h2 class="text-4xl">Front-end Developer</h2>
+  <div class="grid grid-cols-12 p-4">
+    <div
+      class="col-start-2 col-span-6 flex flex-col justify-center text-gray-800"
+    >
+      <h1 class="text-6xl px-4">Matheus Sartoretto</h1>
+      <h2 class="text-4xl px-4">Front-end Developer</h2>
       <div class="mt-8">
         <!-- TODO: transform links into icons with fontawesome -->
-        <a href="https://github.com/strokesws" target="_blank">GitHub</a>
-        <a href="https://linkedin.com/in/matheussartoretto/" target="_blank">
-          LinkedIn
+        <a class="px-4" href="https://github.com/strokesws" target="_blank"
+          ><i class="fab fa-github-alt fa-3x"></i
+        ></a>
+        <a
+          class="px-4"
+          href="https://linkedin.com/in/matheussartoretto/"
+          target="_blank"
+        >
+          <i class="fab fa-linkedin-in fa-3x"></i>
         </a>
       </div>
     </div>
-    <div class="col-span-2 p-4">
+    <div class="col-span-4 flex flex-col justify-center">
       <PolaroidPicture></PolaroidPicture>
     </div>
   </div>
