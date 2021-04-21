69917f8ead258ac874830c5cc4dbfdb159a5a4d1
Matheus Sartoretto <strokesws@gmail.com>
2021-04-21 21:44:23 +0100

    add polaroid component alt text unit test

--- tests/unit/PolaroidPicture.spec.ts
+++ tests/unit/PolaroidPicture.spec.ts
@@ -1,5 +1,4 @@
 import { mount } from '@vue/test-utils';
-
 import PolaroidPicture from '@/components/PolaroidPicture.vue';
 
 describe('PolaroidPicture.vue', () => {
@@ -9,5 +8,14 @@ describe('PolaroidPicture.vue', () => {
 
     const imgSrc = wrapper.find('img').attributes().src;
     expect(imgSrc).toBe(pictureUrl);
+  });
+
+  it('renders the img with altText prop value', async () => {
+    const pictureUrl = 'a';
+    const altText = 'b';
+    const wrapper = mount(PolaroidPicture, { props: { pictureUrl, altText } });
+
+    const imgAlt = wrapper.find('img').attributes().alt;
+    expect(imgAlt).toBe(altText);
   });
 });
