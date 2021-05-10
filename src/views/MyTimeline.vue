d4971f2a03a75e1ea38fbb511ddcd213c06f02c8
Matheus Sartoretto <strokesws@gmail.com>
2021-05-10 21:07:38 +0100

    add styling to divider

--- src/views/MyTimeline.vue
+++ src/views/MyTimeline.vue
@@ -1,13 +1,31 @@
 <template>
   <h1>My Timeline</h1>
   <div class="timeline p-4">
-    <div class="timeline-item flex flex-row-reverse">
-      <div class="timeline-item__info flex-grow">
-        <div class="timeline-item__info-title">My title</div>
-        <div class="timeline-item__info-text">some text</div>
+    <div
+      class="timeline-item flex flex-row-reverse"
+      v-for="(item, index) in items"
+      :key="index"
+    >
+      <div class="timeline-item__info flex-grow my-4">
+        <h2 class="timeline-item__info-title p-4 text-2xl">My title</h2>
+        <p class="timeline-item__info-text p-4">
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Mauris
+          volutpat enim ut tempor sodales. Donec turpis nisl, sollicitudin
+          iaculis volutpat nec, venenatis a dolor. Mauris porttitor posuere
+          consequat. Morbi venenatis tortor nec molestie tincidunt. Suspendisse
+          fringilla nisi ac sapien bibendum laoreet. Curabitur a semper est,
+          eget ullamcorper diam. Proin vel orci eget magna congue ullamcorper
+          sit amet vitae felis. Lorem ipsum dolor sit amet, consectetur
+          adipiscing elit. Donec ligula orci, molestie interdum mollis et,
+          consectetur sit amet ligula. Cras sed nunc orci. Cras nisi dui, mollis
+          in tincidunt a, blandit sed velit. Morbi est risus, lacinia eu
+          pharetra ut, scelerisque ac nulla. Integer mi diam, facilisis et urna
+          nec, pretium finibus quam.
+        </p>
       </div>
-      <div class="timeline-item__divider flex-none p-4">
-        <div class="timeline-item__dot">
+
+      <div class="timeline-item__divider flex flex-col justify-center p-4">
+        <div class="timeline-item__dot p-2">
           <i class="fab fa-github-alt text-xl sm:text-5xl"></i>
         </div>
       </div>
@@ -15,19 +33,26 @@
   </div>
 </template>
 
-<script lang="ts"></script>
+<script lang="ts">
+  import { defineComponent } from 'vue';
+
+  export default defineComponent({
+    setup() {
+      const items = [{}, {}, {}, {}];
+
+      return { items };
+    },
+  });
+</script>
 
 <style lang="scss">
   .timeline {
     background: chocolate;
 
     &-item {
-      &__divider {
-        background: brown;
-      }
-
       &__info {
-        max-width: calc(50% - 80px);
+        max-width: calc(50% - 46.5px);
+        min-height: 100px;
 
         &-title {
           background: cadetblue;
@@ -38,8 +63,53 @@
         }
       }
 
+      &__divider {
+        position: relative;
+        background: brown;
+
+        &::before {
+          content: '';
+          position: absolute;
+          top: 0;
+          left: calc(50% - 1px);
+          right: auto;
+          bottom: 0;
+          background: rgba(0, 0, 0, 0.12);
+          height: 100%;
+          width: 2px;
+        }
+      }
+
       &__dot {
+        z-index: 1;
         background: burlywood;
+        border-radius: 100%;
+      }
+
+      &:first-child {
+        .timeline-item__info {
+          margin-top: 0;
+        }
+
+        .timeline-item__divider {
+          justify-content: flex-start;
+          padding-top: 0;
+        }
+      }
+
+      &:last-child {
+        .timeline-item__info {
+          margin-bottom: 0;
+        }
+
+        .timeline-item__divider {
+          justify-content: flex-end;
+          padding-bottom: 0;
+        }
+      }
+
+      &:nth-child(2n) {
+        flex-direction: row;
       }
     }
   }
