7872caf126e6cd6a9e0dacabcf53a765da578e0f
Matheus Sartoretto <strokesws@gmail.com>
2021-05-22 18:40:04 +0100

    add store types

--- src/types.d.ts
+++ src/types.d.ts
@@ -2,3 +2,17 @@ export interface IHeaderMenuItem {
   title: string;
   linkTo: string;
 }
+
+export interface IStoreState {
+  name: string;
+  role: string;
+  picture: string;
+  text: string;
+  experienceList: IExperience[];
+}
+
+export interface IExperience {
+  title: string;
+  icon: string;
+  description: string;
+}
