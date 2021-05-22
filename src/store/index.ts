381083e15b0b31cd0d6cb05f747627dbf21ad5ce
Matheus Sartoretto <strokesws@gmail.com>
2021-05-22 18:41:35 +0100

    add load data action from firestore

--- src/store/index.ts
+++ src/store/index.ts
@@ -1,4 +1,7 @@
 import { createStore } from 'vuex';
+import { getDoc, doc } from 'firebase/firestore';
+
+import { db } from '@/firebase';
 import { IStoreState } from '@/types';
 
 export default createStore({
@@ -9,7 +12,32 @@ export default createStore({
     text: '',
     experienceList: [],
   } as IStoreState,
-  mutations: {},
-  actions: {},
+
+  mutations: {
+    setAboutMe: (state, data) => {
+      state.name = data.name;
+      state.role = data.role;
+      state.picture = data.picture;
+      state.text = data.text;
+    },
+  },
+
+  actions: {
+    /**
+     * Load About Me data from database into store state
+     */
+    loadAboutMe: async ({ commit }) => {
+      const docRef = doc(db, 'about', 'me');
+      const docSnap = await getDoc(docRef);
+
+      if (docSnap.exists()) {
+        const aboutMe = docSnap.data;
+        commit('setAboutMe', aboutMe);
+      } else {
+        console.error('About me document not found');
+      }
+    },
+  },
+
   modules: {},
 });
