d89e5a79f0865fcb07e25c47d07db870721de85b
Matheus Sartoretto <strokesws@gmail.com>
2021-05-22 18:40:56 +0100

    add store state

--- src/store/index.ts
+++ src/store/index.ts
@@ -1,7 +1,14 @@
 import { createStore } from 'vuex';
+import { IStoreState } from '@/types';
 
 export default createStore({
-  state: {},
+  state: {
+    name: '',
+    role: '',
+    picture: '',
+    text: '',
+    experienceList: [],
+  } as IStoreState,
   mutations: {},
   actions: {},
   modules: {},
